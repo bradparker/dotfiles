5d85da980f39d033f14c449df0d6b7b881e37549
Brad Parker <hi@bradparker.com>
2021-06-02 09:44:31 +1000

    Go 'full neovim'

--- home.nix
+++ home.nix
@@ -8,39 +8,15 @@ let
     src = sources.ale;
     meta.homepage = "https://github.com/dense-analysis/ale/";
   };
-  vim-with-packages = pkgs.vim_configurable.customize {
-    name = "vim";
-    vimrcConfig.customRC = builtins.readFile ./programs/vim/vimrc;
-    vimrcConfig.packages.default = with pkgs.vimPlugins; {
-      start = [
-        ale
-        base16-vim
-        bufexplorer
-        editorconfig-vim
-        fugitive
-        fzf-vim
-        fzfWrapper
-        nerdtree
-        repeat
-        vim-abolish
-        vim-commentary
-        vim-gitgutter
-        vim-grammarous
-        vim-multiple-cursors
-        vim-polyglot
-        vim-sensible
-        vim-surround
-        vim-test
-      ];
-    };
-  };
 
-  catDir = dirName: pkgs.lib.pipe dirName [
-    builtins.readDir
-    (pkgs.lib.filterAttrs (_: value: value == "regular"))
-    pkgs.lib.attrNames
-    (pkgs.lib.concatMapStrings (name: builtins.readFile (dirName + "/${name}")))
-  ];
+  catDir = dirName:
+    pkgs.lib.pipe dirName [
+      builtins.readDir
+      (pkgs.lib.filterAttrs (_: value: value == "regular"))
+      pkgs.lib.attrNames
+      (pkgs.lib.concatMapStrings
+        (name: builtins.readFile (dirName + "/${name}")))
+    ];
 
   rufo = pkgs.callPackage ({ buildRubyGem, ruby }:
     buildRubyGem rec {
@@ -48,9 +24,9 @@ let
       gemName = "rufo";
       version = "0.12.0";
       source.sha256 = "0nwasskcm0nrf7f52019x4fvxa5zckj4fcvf4cdl0qflrcwb1l9f";
-    }) {};
+    }) { };
 
-  swiftlint = pkgs.callPackage (import ./packages/swiftlint) {};
+  swiftlint = pkgs.callPackage (import ./packages/swiftlint) { };
 
   clone = { runtimeShell, writeScriptBin }:
     writeScriptBin "clone" ''
@@ -153,29 +129,25 @@ let
     '';
 
   scripts = {
-    gifit = pkgs.callPackage gifit {};
+    gifit = pkgs.callPackage gifit { };
 
-    clone = pkgs.callPackage clone {};
-    git-wipped = pkgs.callPackage git-wipped {};
-    git-wip = pkgs.callPackage git-wip {};
-    git-unwip = pkgs.callPackage git-unwip {
-      git-wipped = scripts.git-wipped;
-    };
-    git-amend-wip = pkgs.callPackage git-amend-wip {
-      git-wipped = scripts.git-wipped;
-    };
+    clone = pkgs.callPackage clone { };
+    git-wipped = pkgs.callPackage git-wipped { };
+    git-wip = pkgs.callPackage git-wip { };
+    git-unwip = pkgs.callPackage git-unwip { git-wipped = scripts.git-wipped; };
+    git-amend-wip =
+      pkgs.callPackage git-amend-wip { git-wipped = scripts.git-wipped; };
     git-fixup = pkgs.callPackage git-fixup {
       git-wip = scripts.git-wip;
       git-unwip = scripts.git-unwip;
     };
-    git-trim = pkgs.callPackage git-trim {};
+    git-trim = pkgs.callPackage git-trim { };
   };
 
-  in-time-zone = pkgs.callPackage (import (builtins.fetchTarball {
-    url = https://gist.github.com/bradparker/49d8a97247641cfefd5d07717983eb74/archive/235170823472a8a47b4b94a468d28939a13c1db9.tar.gz;
-  })) {};
-in
-rec {
+  in-time-zone = pkgs.callPackage (import (builtins.fetchTarball
+    "https://gist.github.com/bradparker/49d8a97247641cfefd5d07717983eb74/archive/235170823472a8a47b4b94a468d28939a13c1db9.tar.gz"))
+    { };
+in rec {
   imports = [
     ./programs/termonad/module.nix
     ./modules/roboto-fonts.nix
@@ -199,22 +171,23 @@ rec {
 
   programs.bash = {
     enable = true;
-    sessionVariables = {
-      NIX_PATH = "nixpkgs=${sources.nixpkgs}";
-    };
+    sessionVariables = { NIX_PATH = "nixpkgs=${sources.nixpkgs}"; };
     initExtra = ''
       ${catDir ./programs/bash}
 
       eval "$(${pkgs.coreutils}/bin/dircolors)"
 
       if [ ! -z $BASE16_THEME ]; then
-        source ${builtins.fetchTarball {
-          url = "https://github.com/fnune/base16-fzf/archive/ef4c386689f18bdc754a830a8e66bc2d46d515ae.tar.gz";
-          sha256 = "1hcr9sq3bxnin2b1pn9dzw39ddxsx1a0fr075l62yn9203fvq0hq";
-        }}/bash/base16-$BASE16_THEME.config
+        source ${
+          builtins.fetchTarball {
+            url =
+              "https://github.com/fnune/base16-fzf/archive/ef4c386689f18bdc754a830a8e66bc2d46d515ae.tar.gz";
+            sha256 = "1hcr9sq3bxnin2b1pn9dzw39ddxsx1a0fr075l62yn9203fvq0hq";
+          }
+        }/bash/base16-$BASE16_THEME.config
       fi
     '' + pkgs.lib.optionalString pkgs.stdenv.isLinux ''
-        export LOCALE_ARCHIVE=/usr/lib/locale/locale-archive
+      export LOCALE_ARCHIVE=/usr/lib/locale/locale-archive
     '';
   };
 
@@ -223,9 +196,10 @@ rec {
     escapeTime = 0;
     extraConfig = let
       shellPackage = pkgs.bashInteractive;
-      defaultCommand = if pkgs.stdenv.isDarwin
-        then "exec ${pkgs.reattach-to-user-namespace}/bin/reattach-to-user-namespace -l ${shellPackage}/bin/bash"
-        else "exec ${shellPackage}/bin/bash";
+      defaultCommand = if pkgs.stdenv.isDarwin then
+        "exec ${pkgs.reattach-to-user-namespace}/bin/reattach-to-user-namespace -l ${shellPackage}/bin/bash"
+      else
+        "exec ${shellPackage}/bin/bash";
     in ''
       set-option -g default-command '${defaultCommand}'
 
@@ -233,7 +207,7 @@ rec {
     '';
     newSession = true;
     package = pkgs.tmux;
-    plugins = [];
+    plugins = [ ];
     terminal = "screen-256color";
   };
 
@@ -289,21 +263,55 @@ rec {
 
   programs.bat = {
     enable = true;
-    config = {
-      theme = "base16";
-    };
+    config = { theme = "base16"; };
   };
 
   programs.neovim = {
     enable = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+
     extraConfig = builtins.readFile ./programs/vim/vimrc;
     plugins = with pkgs.vimPlugins; [
-      ale
-      base16-vim
+      {
+        plugin = ale;
+        config = ''
+          let g:ale_linters = {
+          \   'haskell': ['hlint'],
+          \   'javascript': ['eslint'],
+          \   'ruby': ['rubocop'],
+          \}
+
+          let g:ale_fixers = {
+          \   'elm': ['format'],
+          \   'haskell': ['ormolu'],
+          \   'javascript': ['prettier'],
+          \   'ruby': ['rubocop'],
+          \   'typescript': ['prettier'],
+          \}
+
+          let g:ale_completion_enabled = 1
+          let g:ale_fix_on_save = 1
+        '';
+      }
+      {
+        plugin = base16-vim;
+        config = ''
+          if filereadable(expand("~/.vimrc_background"))
+            let base16colorspace=256
+            source ~/.vimrc_background
+          endif
+        '';
+      }
       bufexplorer
       editorconfig-vim
       fugitive
-      fzf-vim
+      {
+        plugin = fzf-vim;
+        config = ''
+          nmap <C-P> :FZF<CR>
+        '';
+      }
       fzfWrapper
       nerdtree
       repeat
@@ -325,8 +333,10 @@ rec {
     profiles = {
       "aoc" = {
         fingerprint = {
-          HDMI-1 = "00ffffffffffff0005e390273f0801001e1d0103803c22782a67a1a5554da2270e5054bfef00d1c0b30095008180814081c001010101a36600a0f0701f803020350055502100001a565e00a0a0a029503020350055502100001e000000fc005532373930420a202020202020000000fd0017501e631e000a2020202020200188020326f14b101f05140413031202110123090707830100006d030c002000383c200060010203023a801871382d40582c450055502100001e011d007251d01e206e28550055502100001e8c0ad08a20e02d10103e96005550210000184d6c80a070703e8030203a0055502100001a000000000000000000000000000000000098";
-          eDP-1 = "00ffffffffffff00387000000000000000190104a51d117806de50a3544c99260f5054000000010101010101010101010101010101011a3680a070381f403020350026a510000018542b80a070381f403020350026a510000018000000100000000000000000000000000000000000fc004c433133334c46324c30310a200022";
+          HDMI-1 =
+            "00ffffffffffff0005e390273f0801001e1d0103803c22782a67a1a5554da2270e5054bfef00d1c0b30095008180814081c001010101a36600a0f0701f803020350055502100001a565e00a0a0a029503020350055502100001e000000fc005532373930420a202020202020000000fd0017501e631e000a2020202020200188020326f14b101f05140413031202110123090707830100006d030c002000383c200060010203023a801871382d40582c450055502100001e011d007251d01e206e28550055502100001e8c0ad08a20e02d10103e96005550210000184d6c80a070703e8030203a0055502100001a000000000000000000000000000000000098";
+          eDP-1 =
+            "00ffffffffffff00387000000000000000190104a51d117806de50a3544c99260f5054000000010101010101010101010101010101011a3680a070381f403020350026a510000018542b80a070381f403020350026a510000018000000100000000000000000000000000000000000fc004c433133334c46324c30310a200022";
         };
         config = {
           DP-1.enable = false;
@@ -356,16 +366,20 @@ rec {
 
   home.file.".config/base16-shell" = {
     source = builtins.fetchTarball {
-      url = "https://github.com/chriskempson/base16-shell/archive/ce8e1e540367ea83cc3e01eec7b2a11783b3f9e1.tar.gz";
+      url =
+        "https://github.com/chriskempson/base16-shell/archive/ce8e1e540367ea83cc3e01eec7b2a11783b3f9e1.tar.gz";
       sha256 = "1yj36k64zz65lxh28bb5rb5skwlinixxz6qwkwaf845ajvm45j1q";
     };
   };
 
   home.file.".local/share/git-completion.bash" = {
-    source = "${builtins.fetchTarball {
-      url = "https://github.com/git/git/archive/2befe97201e1f3175cce557866c5822793624b5a.tar.gz";
-      sha256 = "1mz0arnnd715jl891yg8hjplkm4hgn7pxhwfva5lbda801nps2r7";
-    }}/contrib/completion/git-completion.bash";
+    source = "${
+        builtins.fetchTarball {
+          url =
+            "https://github.com/git/git/archive/2befe97201e1f3175cce557866c5822793624b5a.tar.gz";
+          sha256 = "1mz0arnnd715jl891yg8hjplkm4hgn7pxhwfva5lbda801nps2r7";
+        }
+      }/contrib/completion/git-completion.bash";
   };
 
   home.file.".editorconfig".text = ''
@@ -381,42 +395,40 @@ rec {
 
   home.file.".ghci".text = builtins.readFile ./programs/ghc/ghci;
 
-  home.packages = with pkgs; [
-    bash-completion
-    bashInteractive
-    cabal-install
-    coreutils
-    dnsutils
-    emv
-    entr
-    fd
-    fzf
-    ghc
-    git
-    haskellPackages.ghcid
-    haskellPackages.hlint
-    htop
-    hyperfine
-    in-time-zone
-    ipcalc
-    jq
-    libossp_uuid
-    lynx
-    ncdu
-    netcat
-    nmap
-    ormolu
-    pgformatter
-    ripgrep
-    rufo
-    tig
-    tree
-    vim-with-packages
-    vulnix
-  ] ++ lib.optionals (builtins.currentSystem != "x86_64-darwin") [
-    xclip
-    xsel
-  ] ++ lib.optionals (builtins.currentSystem == "x86_64-darwin") [
-    swiftlint
-  ] ++ pkgs.lib.attrValues scripts;
+  home.packages = with pkgs;
+    [
+      bash-completion
+      bashInteractive
+      cabal-install
+      coreutils
+      dnsutils
+      emv
+      entr
+      fd
+      fzf
+      ghc
+      git
+      haskellPackages.ghcid
+      haskellPackages.hlint
+      htop
+      hyperfine
+      in-time-zone
+      ipcalc
+      jq
+      libossp_uuid
+      lynx
+      ncdu
+      netcat
+      nmap
+      ormolu
+      pgformatter
+      ripgrep
+      rufo
+      tig
+      tree
+      vulnix
+    ]
+    ++ lib.optionals (builtins.currentSystem != "x86_64-darwin") [ xclip xsel ]
+    ++ lib.optionals (builtins.currentSystem == "x86_64-darwin") [ swiftlint ]
+    ++ pkgs.lib.attrValues scripts;
 }
