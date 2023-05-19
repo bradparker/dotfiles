ca9096923f873efbca21239ec98b115bc5b80b5b
Brad Parker <hi@bradparker.com>
2023-05-19 16:00:54 +1000

    Add ~/Code/ABC dir to CDPATH

--- programs/bash/codecdpath.sh
+++ programs/bash/codecdpath.sh
@@ -1,4 +1,13 @@
+add_to_cdpath () {
+  local path=$1
+
+  if [ -d "$path" ] && [[ ! "$CDPATH" == *"$path"* ]]; then
+    export CDPATH="$CDPATH:$path"
+  fi
+}
+
 CODE_DIR="$HOME/Code"
-if [ -d "$CODE_DIR" ] && [[ ! "$CDPATH" == *"$CODE_DIR"* ]]; then
-  export CDPATH="$CDPATH:$CODE_DIR"
-fi
+add_to_cdpath "$CODE_DIR"
+
+ABC_CODE_DIR="$CODE_DIR/ABC"
+add_to_cdpath "$ABC_CODE_DIR"
